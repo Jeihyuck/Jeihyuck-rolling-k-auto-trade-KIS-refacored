37c0c55ea2699c90247b994b5b50d0ecd0ec7e1f
Jeihyuck <73269384+Jeihyuck@users.noreply.github.com>
2025-07-06 02:24:27 +0000

    fix: remove duplicate nested rolling_k_auto_trade_api folder

--- rolling_k_auto_trade_api/rolling_k_auto_trade_api/__init__.py
+++
@@ -1,3 +0,0 @@
-import logging
-
-logging.basicConfig(level=logging.INFO, format="%(levelname)s:%(name)s:%(message)s")
--- rolling_k_auto_trade_api/rolling_k_auto_trade_api/rolling_k_utils.py
+++
@@ -1,5 +0,0 @@
-def calculate_target_price(prev_high, prev_low, today_open, k):
-    """
-    K 돌파 타겟 가격 계산 함수
-    """
-    return today_open + (prev_high - prev_low) * k
